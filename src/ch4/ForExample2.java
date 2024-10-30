ad379914f4fe5de109d4c4a4101699e833641435
Admin <Admin@DESKTOP-52351T8>
2024-10-30 18:01:24 +0900

    241030일 ch5 수업한내용

--- src/ch4/ForExample2.java
+++ src/ch4/ForExample2.java
@@ -4,16 +4,16 @@ public class ForExample2 {
 
 	public static void main(String[] args) {
 		//  다중 for
-		// 2중 for문
-		for (int i = 0; i <5; i ++)  { //상위 for문
-			for(int j = 0; j < 5; j++) { //하위 for문
-//				System.out.println("i가 " +i+"이고, j가 "+j+"인 경우 ");
+		// 2중 for 문
+		for (int i = 0; i <5; i ++)  { //상위 for 문
+			for(int j = 0; j < 5; j++) { //하위 for 문
+//				System.out.println("i 가 " +i+"이고, j 가 "+j+"인 경우 ");
 				System.out.print(i+j+" ");
 			}
 			System.out.println();
 		}
 		
-		// 3중 for문
+		// 3중 for 문
 		for (int i = 0; i < 10; i++) {
 			System.out.println("==================================== 바깥쪽 반복문 : "+i);
 			for (int j = 0 ; j <10; j++) {
@@ -48,7 +48,7 @@ public class ForExample2 {
 				    
 				 }
     	  	  	     
-				 // 초기화 식에서 부동 소수점을 쓰는 float타입을 사용하지 않도록 주의
+				 // 초기화 식에서 부동 소수점을 쓰는 float 타입을 사용하지 않도록 주의
 				 for(float x = 0.1f; x <= 1.0f; x+=0.1f) {
 					 System.out.println(x);
 				 }
---
+++ src/ch5/Week.java
@@ -0,0 +1,13 @@
+package ch5;
+
+public enum Week {  // 열거형 enum, 열거형 이름 Week
+	//열거 상수 목록 : 제한된 값의 목록을 지정
+	MONDAY,
+	TUSEDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+
+}
